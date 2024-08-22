0b9962607d89aef81fbd442738f8357dded4d053
kkangssu <unikang16@gmail.com>
2024-08-22 09:02:11 +0900

    Update and rename Solution_1984.java to Solution_1984_중간평균값구하기.java

--- Week45/Solution_1984_중간평균값구하기.java
+++ Week45/Solution_1984_중간평균값구하기.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution_1984 {
+public class Solution_1984_중간평균값구하기 {
 
 	static int[] arr = new int[10];
 	
