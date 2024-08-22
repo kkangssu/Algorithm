3723795404f658692fec49c8b742b5176ed485fe
kkangssu <unikang16@gmail.com>
2024-08-22 15:01:53 +0900

    Update Solution_1288_새로운불면증치료법.java

--- Week45/Solution_1288_새로운불면증치료법.java
+++ Week45/Solution_1288_새로운불면증치료법.java
@@ -4,7 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution_1288_새로운불면증치료법 {
-	
+
+	//0~9까지의 숫자를 모두 보는 경우는 몇번째 양을 센 시점인가?
+	//중복을 허용하지 않는 set을 이용해 set 안에 0~9까지 모두 저장된 시점에 반복문 break
 	static int T;
 	static Set<Integer> set;
 
@@ -24,7 +26,7 @@ public class Solution_1288_새로운불면증치료법 {
 					set.add((int) (n%10));
 					n /= 10;
 				}
-				if(set.size() == 10) break;
+				if(set.size() == 10) break;		//n%10의 결과만 set에 저장되므로 set 안에는 0~9까지의 숫자만 저장됨 ->  set의 크기만 구하면 된다
 				numSheep++;
 			}
 			sb.append("#" + t + " " + num*numSheep + "\n");
