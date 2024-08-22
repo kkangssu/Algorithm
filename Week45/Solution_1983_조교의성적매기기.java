7fbd0d8879228eb95c6d7de646983a0cc64d2368
kkangssu <unikang16@gmail.com>
2024-08-22 15:28:32 +0900

    Update Solution_1983_조교의성적매기기.java

--- Week45/Solution_1983_조교의성적매기기.java
+++ Week45/Solution_1983_조교의성적매기기.java
@@ -13,37 +13,36 @@ public class Solution_1983_조교의성적매기기 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        	StringBuilder sb = new StringBuilder();
         
-        T = Integer.parseInt(br.readLine());
-        for (int t = 1; t <= T; t++) {
-        	StringTokenizer st = new StringTokenizer(br.readLine());
-        	N = Integer.parseInt(st.nextToken());
-        	K = Integer.parseInt(st.nextToken());
-        	int scoreK = 0;
-        	int num = N/10;
-        	score = new int[N];
-        	for (int i = 0; i < N; i++) {
-        		st = new StringTokenizer(br.readLine());
-        		score[i] = 35*Integer.parseInt(st.nextToken())
+       		T = Integer.parseInt(br.readLine());
+        	for (int t = 1; t <= T; t++) {
+        		StringTokenizer st = new StringTokenizer(br.readLine());
+        		N = Integer.parseInt(st.nextToken());
+        		K = Integer.parseInt(st.nextToken());
+        		int scoreK = 0;		//K번째 학생의 점수
+        		int num = N/10;
+        		score = new int[N];
+        		for (int i = 0; i < N; i++) {
+        			st = new StringTokenizer(br.readLine());
+        			score[i] = 35*Integer.parseInt(st.nextToken())
         					+ 45*Integer.parseInt(st.nextToken())
         						+ 20*Integer.parseInt(st.nextToken());
-        		if(i == K-1) scoreK = score[i];
+        			if(i == K-1) scoreK = score[i];	
 			}
-        	//System.out.println(scoreK);
-        	int group = 0;
-        	Arrays.sort(score);
-        	//System.out.println(Arrays.toString(score));
-        	for (int i = 0; i < N; i+=num) {
+        		int group = 0;
+        		Arrays.sort(score);
+        		for (int i = 0; i < N; i+=num) {
+				//i번째 학생은 각 성적 그룹의 가장 점수가 높은 학생
+				//i번 학생보다 성적이 높으면 i번 학생보다 성적이 한 단계 높은 것
 				if(scoreK <= score[N-i-1]) continue;
 				else if(scoreK > score[N-i-1]) {
 					group = i/num-1;
-					//System.out.println((N-i-1) + " " + score[N-i-1]+ " "+ group);
 					break;
 				}
 			}
         	sb.append("#" + t + " " + alpha[group] + "\n");
-        }
-        System.out.println(sb.toString());
+        	}
+        	System.out.println(sb.toString());
 	}
 }
