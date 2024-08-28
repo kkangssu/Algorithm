35c9031092622e048dc28571122244388c1635b7
kkangssu <unikang16@gmail.com>
2024-08-28 11:39:39 +0900

    Add files via upload

--- Week6/Main_6603_로또.java
+++ Week6/Main_6603_로또.java
@@ -6,52 +6,52 @@ import java.util.StringTokenizer;
 
 public class Main_6603_로또 {
 	
-	public static int[] num;
-	public static StringBuilder sb = new StringBuilder();
-	public static int[] arr;
-	public static boolean[] pick;
+	static int[] num;
+	static int[] arr;
+	static boolean[] visited;
+	static StringBuilder sb;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		String str;
+		sb = new StringBuilder();
 		
-		 for(int i = 0; ; i++) {
-			str = br.readLine();
+		 while(true) {
+			String str = br.readLine();
 			if(str.equals("0")) {
 				break;
 			}
-			else {
-				st = new StringTokenizer(str, " ");
-				int k = Integer.parseInt(st.nextToken());
-				num = new int[k];
-				arr = new int[k];
-				pick = new boolean[k];
-				for(int j = 0; j < k; j++) {
-					num[j] = Integer.parseInt(st.nextToken());
-				}
-				lotto(k, 0, -1);
+			
+			st = new StringTokenizer(str);
+			int k = Integer.parseInt(st.nextToken());
+			num = new int[k];
+			arr = new int[k];
+			visited = new boolean[k];
+			for(int j = 0; j < k; j++) {
+				num[j] = Integer.parseInt(st.nextToken());
 			}
+			lotto(k, 0, -1);
 			sb.append("\n");
 		 }
 
-		 System.out.println(sb);
+		 System.out.println(sb.toString());
 	}
 	
 	public static void lotto(int k, int dp, int prev) {
+		
 		if(dp == 6) {
 			for(int i = 0; i < 6; i++) {
-				sb.append(arr[i] + " ");
+				sb.append(arr[i]).append(" ");
 			}
 			sb.append("\n");
 		}
 		
 		for(int i = prev+1; i < k; i++) {
-			if(!pick[i]) {
+			if(!visited[i]) {
 				arr[dp] = num[i];
-				pick[i] = true;
+				visited[i] = true;
 				lotto(k, dp+1, i);
-				pick[i] = false;
+				visited[i] = false;
 			}
 		}
 	}
