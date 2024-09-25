393e88537b941f3bbb31c3d659f6910826d76b47
kkangssu <unikang16@gmail.com>
2024-09-26 00:21:16 +0900

    Add files via upload

--- 2409_week04/Main_1477_휴게소세우기.java
+++ 2409_week04/Main_1477_휴게소세우기.java
@@ -1,4 +1,4 @@
-package s09_week4;
+package s2409_w3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,7 +9,7 @@ public class Main_1477_휴게소세우기 {
 	
 	static int N, M, L;
 	static int[] rest;
-	static int[] interval;
+	static int left, right;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,15 +19,35 @@ public class Main_1477_휴게소세우기 {
 		M = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
 		
-		rest = new int[N];
+		rest = new int[N+2];
+		rest[0] = 0;
+		rest[N+1] = L;
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			rest[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(rest);
-		System.out.println(Arrays.toString(rest));
 		
-		//휴게소 간 간격 구하기 -> interval[]
+		BS();
+		
+		System.out.println(left);
 	}
 
+	static void BS() {
+		left = 1;
+		right = L;
+		
+		while(left <= right) {
+			int interval = (left + right) / 2;
+			int cnt = 0;
+			
+			for (int i = 1; i < N+2; i++) {
+				int tmp = rest[i] - rest[i-1] - 1;
+				cnt += tmp / interval;
+			}
+			
+			if(cnt > M) left = interval + 1;
+			else right = interval - 1;
+		}
+	}
 }
