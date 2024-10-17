a7ba5eb804f092d8976d2ff5f84e0bb55a39c05a
kkangssu <unikang16@gmail.com>
2024-10-17 18:00:09 +0900

    Add files via upload

---
+++ 2410_week03/Main_9251_LCS.java
@@ -0,0 +1,34 @@
+package s10_w3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_9251_LCS {
+	
+	static int[][] dp;
+	static char[] str1;
+	static char[] str2;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		str1 = br.readLine().toCharArray();
+		str2 = br.readLine().toCharArray();
+		dp = new int[str1.length+1][str2.length+1];
+		
+		for (int i = 0; i < str1.length; i++) {
+			for (int j = 0; j < str2.length; j++) {
+				if(str1[i] != str2[j]) {
+					dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
+				}
+				else {
+					dp[i+1][j+1] = dp[i][j] + 1;
+				}
+			}
+		}
+		
+		System.out.println(dp[str1.length][str2.length]);
+
+	}
+
+}
