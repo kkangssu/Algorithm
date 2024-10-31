e4cc295eb24399ed0194447928fa1b547be16700
kkangssu <unikang16@gmail.com>
2024-10-31 17:52:00 +0900

    Update Main_2565_전깃줄.java

--- 2410_week05/Main_2565_전깃줄.java
+++ 2410_week05/Main_2565_전깃줄.java
@@ -24,7 +24,8 @@ public class Main_2565_전깃줄 {
 	static Wire[] wire;
 	static int[] dp;
 	static int max;
-
+	//일단 dp로 풀려면 정렬이 되어 있어야 함 -> 시작점을 기준으로 정렬하기 위해 class 생성
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -42,14 +43,17 @@ public class Main_2565_전깃줄 {
 		
 		for (int i = 1; i < N; i++) {
 			dp[i] = 1;
+			//자기자신만 연결된다 생각하면 dp 최소값은 1
 			for (int j = 0; j < i; j++) {
+				//교차된다면
 				if(wire[i].e > wire[j].e) {
 					dp[i] = Math.max(dp[i], dp[j]+1);
 				}
 			}
+			//연결할 수 있는 전깃출 최대값 계산
 			max = Math.max(max, dp[i]);
 		}
-		
+		//없애야하는 전깃줄 최소값
 		System.out.println(N - max);
 	}
 
